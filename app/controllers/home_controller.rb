cd229f617f4aae4d367e75b43f11690deb226bb4
thalytalima211 <thalytalima211@gmail.com>
2024-04-22 20:43:19 -0300

    Vendo buffets na página principal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,5 @@
 class HomeController < ApplicationController
-  before_action :redirect_profile, only: [:index]
   def index
-  end
-
-  private
-
-  def redirect_profile
-    if user_signed_in?
-
-    elsif admin_signed_in? && current_admin.buffet.present?
-        redirect_to buffet_path(current_admin.buffet)
-    end
+    @buffets = Buffet.all
   end
 end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -16,4 +16,22 @@ describe 'Usuário entra na página inicial' do
     expect(page).to have_link 'Entrar como cliente'
     expect(page).to have_link 'Entrar como administrador'
   end
+
+  it 'e vê buffets cadastrados' do
+    # Arrange
+    admin = Admin.create!(email: 'admin@email.com', password: 'senha123')
+    buffet = Buffet.create!(corporate_name: 'Sabores Divinos Eventos Ltda.', brand_name: 'Sabores Divinos Buffet',
+                            registration_number: '12.345.678/0001-90', number_phone: '(55)5555-5555',
+                            email: 'contato@saboresdivinos.com',  full_address: 'Av. das Delícias, 1234',
+                            neighborhood: 'Centro', city: 'São Paulo', state: 'SP', zip_code: '01234-567',
+                            description: 'Sabores Divinos Buffet é especializado em transformar eventos em experiências inesquecíveis',
+                            admin: admin)
+    # Act
+    visit root_path
+
+    # Assert
+    expect(page).to have_content 'Sabores Divinos Buffet'
+    expect(page).to have_content 'São Paulo'
+    expect(page).to have_content 'SP'
+  end
 end
